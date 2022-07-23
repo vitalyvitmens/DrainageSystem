ecd2d503634d219faa4a63a08a22ed16dd9f192d
vitalyvitmens <otm_@tut.by>
2022-07-23 23:25:17 +0300

    дописал методы расчета профнастила и штакета в разных толщинах и с разным покрытием + аннотации к ним. Преопределил метод toString для классов class PriceDrainage и PriceHedge, для удобного вывода в консоль их свойств

--- hedge.dart
+++ hedge.dart
@@ -235,17 +235,130 @@ class ProfileMP20 extends Hedge {
     return areaMP20;
   }
 
-  get_MP20_cost() {
+  get_MP20_cost_050mat() {
     /*
-  Метод get_MP20_cost позволяет получить / рассчитать стоимость 
-  листов профнастила MП20 для изгороди, на основании:
-  - double get_sheets_areaMP20() (метод расчета площади профнастила MП20 в м2)
-  - double PriceHedge().MP20_price (параметр класса PriceHedge: 
-  стоимость профнастила MП20 за 1м2 в руб)
-  возвращает double MP20_cost (общая стоимость всех листов профнастила MП20 в руб)
+  Метод get_MP20_cost_050mat позволяет получить / рассчитать стоимость матовых 
+  листов профнастила МП20 для изгороди в 0.50мм толщине, на основании:
+  - double get_sheets_areaMP20() (метод расчета площади профнастила МП20 в м2)
+  - double PriceHedge().MP20_price_050mat (параметр класса PriceHedge: 
+  стоимость матового профнастила МП20 в 0.50мм толщине за 1м2 в руб)
+  возвращает double MP20_cost_050mat (общая стоимость всех матовых листов 
+  профнастила МП20 в 0.50мм толщине в руб)
   */
-    double MP20_cost = get_areaMP20() * PriceHedge().MP20_price;
-    return MP20_cost;
+    double MP20_cost_050mat = get_areaMP20() * PriceHedge().MP20_price_050mat;
+    return MP20_cost_050mat;
+  }
+
+  get_MP20_cost_045mat() {
+    /*
+  Метод get_MP20_cost_045mat позволяет получить / рассчитать стоимость матовых 
+  листов профнастила МП20 для изгороди в 0.45мм толщине, на основании:
+  - double get_sheets_areaMP20() (метод расчета площади профнастила МП20 в м2)
+  - double PriceHedge().MP20_price_045mat (параметр класса PriceHedge: 
+  стоимость матового профнастила МП20 в 0.45мм толщине за 1м2 в руб)
+  возвращает double MP20_cost_045mat (общая стоимость всех матовых листов 
+  профнастила МП20 в 0.45мм толщине в руб)
+  */
+    double MP20_cost_045mat = get_areaMP20() * PriceHedge().MP20_price_045mat;
+    return MP20_cost_045mat;
+  }
+
+  get_MP20_cost_050() {
+    /*
+  Метод get_MP20_cost_050 позволяет получить / рассчитать стоимость глянцевых 
+  листов профнастила МП20 для изгороди в 0.50мм толщине, на основании:
+  - double get_sheets_areaMP20() (метод расчета площади профнастила МП20 в м2)
+  - double PriceHedge().MP20_price_050 (параметр класса PriceHedge: 
+  стоимость глянцевого профнастила МП20 в 0.50мм толщине за 1м2 в руб)
+  возвращает double MP20_cost_050 (общая стоимость всех глянцевых листов 
+  профнастила МП20 в 0.50мм толщине в руб)
+  */
+    double MP20_cost_050 = get_areaMP20() * PriceHedge().MP20_price_050;
+    return MP20_cost_050;
+  }
+
+  get_MP20_cost_045() {
+    /*
+  Метод get_MP20_cost_045 позволяет получить / рассчитать стоимость глянцевых 
+  листов профнастила МП20 для изгороди в 0.45мм толщине, на основании:
+  - double get_sheets_areaMP20() (метод расчета площади профнастила МП20 в м2)
+  - double PriceHedge().MP20_price_045 (параметр класса PriceHedge: 
+  стоимость глянцевого профнастила МП20 в 0.45мм толщине за 1м2 в руб)
+  возвращает double MP20_cost_045 (общая стоимость всех глянцевых листов 
+  профнастила МП20 в 0.45мм толщине в руб)
+  */
+    double MP20_cost_045 = get_areaMP20() * PriceHedge().MP20_price_045;
+    return MP20_cost_045;
+  }
+
+  get_MP20_cost_040() {
+    /*
+  Метод get_MP20_cost_040 позволяет получить / рассчитать стоимость глянцевых 
+  листов профнастила МП20 для изгороди в 0.40мм толщине, на основании:
+  - double get_sheets_areaMP20() (метод расчета площади профнастила МП20 в м2)
+  - double PriceHedge().MP20_price_040 (параметр класса PriceHedge: 
+  стоимость глянцевого профнастила МП20 в 0.40мм толщине за 1м2 в руб)
+  возвращает double MP20_cost_040 (общая стоимость всех глянцевых листов 
+  профнастила МП20 в 0.40мм толщине в руб)
+  */
+    double MP20_cost_040 = get_areaMP20() * PriceHedge().MP20_price_040;
+    return MP20_cost_040;
+  }
+
+  get_MP20_cost_035() {
+    /*
+  Метод get_MP20_cost_035 позволяет получить / рассчитать стоимость глянцевых 
+  листов профнастила МП20 для изгороди в 0.35мм толщине, на основании:
+  - double get_sheets_areaMP20() (метод расчета площади профнастила МП20 в м2)
+  - double PriceHedge().MP20_price_035 (параметр класса PriceHedge: 
+  стоимость глянцевого профнастила МП20 в 0.35мм толщине за 1м2 в руб)
+  возвращает double MP20_cost_035 (общая стоимость всех глянцевых листов 
+  профнастила МП20 в 0.35мм толщине в руб)
+  */
+    double MP20_cost_035 = get_areaMP20() * PriceHedge().MP20_price_035;
+    return MP20_cost_035;
+  }
+
+  get_MP20_cost_050zinc() {
+    /*
+  Метод get_MP20_cost_050zinc позволяет получить / рассчитать стоимость 
+  оцинкованных листов профнастила МП20 для изгороди в 0.50мм толщине, на основании:
+  - double get_sheets_areaMP20() (метод расчета площади профнастила МП20 в м2)
+  - double PriceHedge().MP20_price_050zinc (параметр класса PriceHedge: 
+  стоимость оцинкованного профнастила МП20 в 0.50мм толщине за 1м2 в руб)
+  возвращает double MP20_cost_050zinc (общая стоимость всех оцинкованных листов 
+  профнастила МП20 в 0.50мм толщине в руб)
+  */
+    double MP20_cost_050zinc = get_areaMP20() * PriceHedge().MP20_price_050zinc;
+    return MP20_cost_050zinc;
+  }
+
+  get_MP20_cost_045zinc() {
+    /*
+  Метод get_MP20_cost_045zinc позволяет получить / рассчитать стоимость 
+  оцинкованных листов профнастила МП20 для изгороди в 0.45мм толщине, на основании:
+  - double get_sheets_areaMP20() (метод расчета площади профнастила МП20 в м2)
+  - double PriceHedge().MP20_price_045zinc (параметр класса PriceHedge: 
+  стоимость оцинкованного профнастила МП20 в 0.45мм толщине за 1м2 в руб)
+  возвращает double MP20_cost_045zinc (общая стоимость всех оцинкованных листов 
+  профнастила МП20 в 0.45мм толщине в руб)
+  */
+    double MP20_cost_045zinc = get_areaMP20() * PriceHedge().MP20_price_045zinc;
+    return MP20_cost_045zinc;
+  }
+
+  get_MP20_cost_040zinc() {
+    /*
+  Метод get_MP20_cost_040zinc позволяет получить / рассчитать стоимость 
+  оцинкованных листов профнастила МП20 для изгороди в 0.40мм толщине, на основании:
+  - double get_sheets_areaMP20() (метод расчета площади профнастила МП20 в м2)
+  - double PriceHedge().MP20_price_040zinc (параметр класса PriceHedge: 
+  стоимость оцинкованного профнастила МП20 в 0.40мм толщине за 1м2 в руб)
+  возвращает double MP20_cost_040zinc (общая стоимость всех оцинкованных листов 
+  профнастила МП20 в 0.40мм толщине в руб)
+  */
+    double MP20_cost_040zinc = get_areaMP20() * PriceHedge().MP20_price_040zinc;
+    return MP20_cost_040zinc;
   }
 }
 
@@ -290,17 +403,34 @@ class Fence extends Hedge {
     return length_fence;
   }
 
-  get_fence_cost() {
+  get_fence_double_sided_cost_040mat() {
     /*
-  Метод get_fence_cost позволяет получить / рассчитать стоимость 
-  штакетника для изгороди, на основании:
+  Метод get_fence_double_sided_cost_040mat позволяет получить / рассчитать 
+  стоимость двухстороннего матового 0.40мм штакета для изгороди, на основании:
   - double get_length_fence() (метод расчета погонажа штакетника в мп)
-  - double PriceHedge().fence_price (параметр класса PriceHedge: 
-  стоимость штакетника за 1м2 в руб),
-  возвращает double fence_cost (общая стоимость штакетника в руб)
+  - double PriceHedge().fence_double_sided_price_040mat (параметр класса 
+  PriceHedge: стоимость двухстороннего матового 0.40мм штакета за 1м в руб),
+  возвращает double fence_double_sided_cost_040mat (общая стоимость 
+  двухстороннего матового 0.40мм штакета в руб)
   */
-    double fence_cost = get_length_fence() * PriceHedge().fence_price;
-    return fence_cost;
+    double fence_double_sided_cost_040mat =
+        get_length_fence() * PriceHedge().fence_double_sided_price_040mat;
+    return fence_double_sided_cost_040mat;
+  }
+
+  get_fence_double_sided_cost_040() {
+    /*
+  Метод get_fence_double_sided_cost_040 позволяет получить / рассчитать 
+  стоимость двухстороннего глянцевого 0.40мм штакета для изгороди, на основании:
+  - double get_length_fence() (метод расчета погонажа штакетника в мп)
+  - double PriceHedge().fence_double_sided_price_040 (параметр класса 
+  PriceHedge: стоимость двухстороннего глянцевого 0.40мм штакета за 1м в руб),
+  возвращает double fence_double_sided_cost_040 (общая стоимость двухстороннего 
+  глянцевого 0.40мм штакета в руб)
+  */
+    double fence_double_sided_cost_040 =
+        get_length_fence() * PriceHedge().fence_double_sided_price_040;
+    return fence_double_sided_cost_040;
   }
 }
 
@@ -312,24 +442,35 @@ void main() {
 
   print('''
 РАСЧЁТНЫЕ ПАРАМЕТРЫ ЗАБОРА:\n$hedge
-Количество листов С8:    ${profileC8.get_num_sheetsC8()} шт
-Площадь листов С8:       ${profileC8.get_areaC8().toStringAsFixed(3)} м2
-Стоимость С8 МАТ 0.50мм: ${profileC8.get_C8_cost_050mat().toStringAsFixed(2)} руб
-Стоимость С8 МАТ 0.45мм: ${profileC8.get_C8_cost_045mat().toStringAsFixed(2)} руб
-Стоимость С8 0.50мм:     ${profileC8.get_C8_cost_050().toStringAsFixed(2)} руб
-Стоимость С8 0.45мм:     ${profileC8.get_C8_cost_045().toStringAsFixed(2)} руб
-Стоимость С8 0.40мм:     ${profileC8.get_C8_cost_040().toStringAsFixed(2)} руб
-Стоимость С8 0.35мм:     ${profileC8.get_C8_cost_035().toStringAsFixed(2)} руб
-Стоимость С8 Оц 0.50мм:  ${profileC8.get_C8_cost_050zinc().toStringAsFixed(2)} руб
-Стоимость С8 Оц 0.45мм:  ${profileC8.get_C8_cost_045zinc().toStringAsFixed(2)} руб
-Стоимость С8 Оц 0.40мм:  ${profileC8.get_C8_cost_040zinc().toStringAsFixed(2)} руб
-\n
-Количество листов МП20: ${profileMP20.get_num_sheetsMP20()} шт
-Площадь листов МП20:    ${profileMP20.get_areaMP20().toStringAsFixed(3)} м2
-Стоимость МП20:         ${profileMP20.get_MP20_cost().toStringAsFixed(2)} руб
-\n
-Количество штакетин:    ${fence.get_num_fence()} шт
-Погонаж штакетник:      ${fence.get_length_fence().toStringAsFixed(3)} мп
-Стоимость штакетник:    ${fence.get_fence_cost().toStringAsFixed(2)} руб\n
-''');
+Количество листов С8:                   ${profileC8.heigth} м = ${profileC8.get_num_sheetsC8()} шт
+Площадь листов С8:                      ${profileC8.get_areaC8().toStringAsFixed(3)} м2
+Стоимость С8 Матовый 0.50мм:            ${profileC8.get_C8_cost_050mat().toStringAsFixed(2)} руб
+Стоимость С8 Матовый 0.45мм:            ${profileC8.get_C8_cost_045mat().toStringAsFixed(2)} руб
+Стоимость С8 Глянец 0.50мм:             ${profileC8.get_C8_cost_050().toStringAsFixed(2)} руб
+Стоимость С8 Глянец 0.45мм:             ${profileC8.get_C8_cost_045().toStringAsFixed(2)} руб
+Стоимость С8 Глянец 0.40мм:             ${profileC8.get_C8_cost_040().toStringAsFixed(2)} руб
+Стоимость С8 Глянец 0.35мм:             ${profileC8.get_C8_cost_035().toStringAsFixed(2)} руб
+Стоимость С8 Оцинк 0.50мм:              ${profileC8.get_C8_cost_050zinc().toStringAsFixed(2)} руб
+Стоимость С8 Оцинк 0.45мм:              ${profileC8.get_C8_cost_045zinc().toStringAsFixed(2)} руб
+Стоимость С8 Оцинк 0.40мм:              ${profileC8.get_C8_cost_040zinc().toStringAsFixed(2)} руб
+
+Количество листов МП20:                 ${profileMP20.heigth} м = ${profileMP20.get_num_sheetsMP20()} шт
+Площадь листов МП20:                    ${profileMP20.get_areaMP20().toStringAsFixed(3)} м2
+Стоимость МП20 Матовый 0.50мм:          ${profileMP20.get_MP20_cost_050mat().toStringAsFixed(2)} руб
+Стоимость МП20 Матовый 0.45мм:          ${profileMP20.get_MP20_cost_045mat().toStringAsFixed(2)} руб
+Стоимость МП20 Глянец 0.50мм:           ${profileMP20.get_MP20_cost_050().toStringAsFixed(2)} руб
+Стоимость МП20 Глянец 0.45мм:           ${profileMP20.get_MP20_cost_045().toStringAsFixed(2)} руб
+Стоимость МП20 Глянец 0.40мм:           ${profileMP20.get_MP20_cost_040().toStringAsFixed(2)} руб
+Стоимость МП20 Глянец 0.35мм:           ${profileMP20.get_MP20_cost_035().toStringAsFixed(2)} руб
+Стоимость МП20 Оцинк 0.50мм:            ${profileMP20.get_MP20_cost_050zinc().toStringAsFixed(2)} руб
+Стоимость МП20 Оцинк 0.45мм:            ${profileMP20.get_MP20_cost_045zinc().toStringAsFixed(2)} руб
+Стоимость МП20 Оцинк 0.40мм:            ${profileMP20.get_MP20_cost_040zinc().toStringAsFixed(2)} руб
+
+Количество штакетин:                    ${fence.heigth} м = ${fence.get_num_fence()} шт
+Погонаж штакетник:                      ${fence.get_length_fence().toStringAsFixed(3)} мп
+Стоимость 2х матового 0.40мм штакета:   ${fence.get_fence_double_sided_cost_040mat().toStringAsFixed(2)} руб
+Стоимость 2х глянцевого 0.40мм штакета: ${fence.get_fence_double_sided_cost_040().toStringAsFixed(2)} руб''');
+
+  // print('\n${PriceDrainage().toString()}');
+  // print('${PriceHedge().toString()}');
 }
--- price.dart
+++ price.dart
@@ -1,4 +1,21 @@
 class PriceDrainage {
+  /*
+  Класс PriceDrainage (Прайс водосточной системы), он принимает 
+  на вход следующие свойства (параметры):
+  - double gutter_price (стоимость желоба за 1шт. в руб)
+  - double gutter_connector_price (стоимость соединителя желоба за 1шт. в руб)
+  - double plug_price (стоимость заглушки желоба за 1шт. в руб)
+  - double gutter_holder_price (стоимость держателя желоба за 1шт. в руб)
+  - double gutter_angle_price (стоимость угла желоба за 1шт. в руб)
+  - double funnel_price (стоимость воронки за 1шт. в руб)
+  - double elbow_price (стоимость колена за 1шт. в руб)
+  - double big_pipe_price (стоимость большой трубы за 1шт. в руб)
+  - double pipe_connector_price (стоимость соединителя трубы за 1шт. в руб)
+  - double small_pipe_price (стоимость малой трубы за 1шт. в руб)
+  - double tip_price (стоимость отмёта за 1шт. в руб)
+  - double pipe_holder_price (стоимость держателя трубы за 1шт. в руб)
+  - double pin_price (стоимость штыря за 1шт. в руб)
+  */
   double gutter_price = 21.12;
   double gutter_connector_price = 8.28;
   double plug_price = 3.96;
@@ -12,9 +29,52 @@ class PriceDrainage {
   double tip_price = 8.88;
   double pipe_holder_price = 3.84;
   double pin_price = 5.76;
+
+  @override
+  String toString() {
+    return '''
+Стоимость желоба (L=3000):    $gutter_price руб 
+Стоимость соединителя желоба: $gutter_connector_price руб
+Стоимость заглушки желоба:    $plug_price руб
+Стоимость держателя желоба:   $gutter_holder_price руб
+Стоимость угла желоба:        $gutter_angle_price руб
+Стоимость воронки:            $funnel_price руб
+Стоимость колена:             $elbow_price руб
+Стоимость трубы (L=3000):     $big_pipe_price руб
+Стоимость соединителя трубы:  $pipe_connector_price руб
+Стоимость трубы (L=1000):     $small_pipe_price руб
+Стоимость отмёта:             $tip_price руб
+Стоимость держателя трубы:    $pipe_holder_price руб
+Стоимость штыря:              $pin_price руб
+''';
+  }
 }
 
 class PriceHedge {
+  /*
+  Класс PriceHedge (Прайс Изгородь), он принимает 
+  на вход следующие свойства (параметры):
+  - double C8_price_050mat (стоимость матового 0.50мм профнастила С8 за 1м2 в руб)
+  - double C8_price_045mat (стоимость матового 0.45мм профнастила С8 за 1м2 в руб)
+  - double C8_price_050 (стоимость глянцевого 0.50мм профнастила С8 за 1м2 в руб)
+  - double C8_price_045 (стоимость глянцевого 0.45мм профнастила С8 за 1м2 в руб)
+  - double C8_price_040 (стоимость глянцевого 0.40мм профнастила С8 за 1м2 в руб)
+  - double C8_price_035 (стоимость глянцевого 0.35мм профнастила С8 за 1м2 в руб)
+  - double C8_price_050zinc (стоимость оцинкованного 0.50мм профнастила С8 за 1м2 в руб)
+  - double C8_price_045zinc (стоимость оцинкованного 0.45мм профнастила С8 за 1м2 в руб)
+  - double C8_price_040zinc (стоимость оцинкованного 0.40мм профнастила С8 за 1м2 в руб)
+  - double MP20_price_050mat (стоимость матового 0.50мм профнастила MП20 за 1м2 в руб)
+  - double MP20_price_045mat (стоимость матового 0.45мм профнастила MП20 за 1м2 в руб)
+  - double MP20_price_050 (стоимость глянцевого 0.50мм профнастила MП20 за 1м2 в руб)
+  - double MP20_price_045 (стоимость глянцевого 0.45мм профнастила MП20 за 1м2 в руб)
+  - double MP20_price_040 (стоимость глянцевого 0.40мм профнастила MП20 за 1м2 в руб)
+  - double MP20_price_035 (стоимость глянцевого 0.35мм профнастила MП20 за 1м2 в руб)
+  - double MP20_price_050zinc (стоимость оцинкованного 0.50мм профнастила MП20 за 1м2 в руб)
+  - double MP20_price_045zinc (стоимость оцинкованного 0.45мм профнастила MП20 за 1м2 в руб)
+  - double MP20_price_040zinc (стоимость оцинкованного 0.40мм профнастила MП20 за 1м2 в руб)
+  - double fence_double_sided_price_040mat (стоимость двухстороннего матового 0.40мм штакета за 1м в руб)
+  - double fence_double_sided_price_040 (стоимость двухстороннего глянцевого 0.40мм штакета за 1м в руб)
+  */
   double C8_price_050mat = 21.24;
   double C8_price_045mat = 18.60;
   double C8_price_050 = 18.00;
@@ -24,6 +84,41 @@ class PriceHedge {
   double C8_price_050zinc = 15.18;
   double C8_price_045zinc = 14.04;
   double C8_price_040zinc = 11.18;
-  double MP20_price = 17.10;
-  double fence_price = 2.30;
+  double MP20_price_050mat = 22.80;
+  double MP20_price_045mat = 19.32;
+  double MP20_price_050 = 18.60;
+  double MP20_price_045 = 17.10;
+  double MP20_price_040 = 16.08;
+  double MP20_price_035 = 12.92;
+  double MP20_price_050zinc = 15.84;
+  double MP20_price_045zinc = 14.52;
+  double MP20_price_040zinc = 12.36;
+  double fence_double_sided_price_040mat = 2.70;
+  double fence_double_sided_price_040 = 2.30;
+
+  @override
+  String toString() {
+    return '''
+Стоимость матового 0.50мм профнастила С8 за 1м2:          ${C8_price_050mat.toStringAsFixed(2)} руб 
+Стоимость матового 0.45мм профнастила С8 за 1м2:          ${C8_price_045mat.toStringAsFixed(2)} руб
+Стоимость глянцевого 0.50мм профнастила С8 за 1м2:        ${C8_price_050.toStringAsFixed(2)} руб
+Стоимость глянцевого 0.45мм профнастила С8 за 1м2:        ${C8_price_045.toStringAsFixed(2)} руб
+Стоимость глянцевого 0.40мм профнастила С8 за 1м2:        ${C8_price_040.toStringAsFixed(2)} руб
+Стоимость глянцевого 0.35мм профнастила С8 за 1м2:        ${C8_price_035.toStringAsFixed(2)} руб
+Стоимость оцинкованного 0.50мм профнастила С8 за 1м2:     ${C8_price_050zinc.toStringAsFixed(2)} руб
+Стоимость оцинкованного 0.45мм профнастила С8 за 1м2:     ${C8_price_045zinc.toStringAsFixed(2)} руб
+Стоимость оцинкованного 0.40мм профнастила С8 за 1м2:     ${C8_price_040zinc.toStringAsFixed(2)} руб
+Стоимость матового 0.50мм профнастила MП20 за 1м2:        ${MP20_price_050mat.toStringAsFixed(2)} руб
+Стоимость матового 0.45мм профнастила MП20 за 1м2:        ${MP20_price_045mat.toStringAsFixed(2)} руб
+Стоимость глянцевого 0.50мм профнастила MП20 за 1м2:      ${MP20_price_050.toStringAsFixed(2)} руб
+Стоимость глянцевого 0.45мм профнастила MП20 за 1м2:      ${MP20_price_045.toStringAsFixed(2)} руб
+Стоимость глянцевого 0.40мм профнастила MП20 за 1м2:      ${MP20_price_040.toStringAsFixed(2)} руб
+Стоимость болглянцевого 0.35мм профнастила MП20 за 1м2:   ${MP20_price_035.toStringAsFixed(2)} руб
+Стоимость оцинкованного 0.50мм профнастила MП20 за 1м2:   ${MP20_price_050zinc.toStringAsFixed(2)} руб
+Стоимость оцинкованного 0.45мм профнастила MП20 за 1м2:   ${MP20_price_045zinc.toStringAsFixed(2)} руб
+Стоимость оцинкованного 0.40мм профнастила MП20 за 1м2:   ${MP20_price_040zinc.toStringAsFixed(2)} руб
+Стоимость двухстороннего матового 0.40мм штакета за 1м:   ${fence_double_sided_price_040mat.toStringAsFixed(2)} руб
+Стоимость двухстороннего глянцевого 0.40мм штакета за 1м: ${fence_double_sided_price_040.toStringAsFixed(2)} руб
+''';
+  }
 }
